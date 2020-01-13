4bb912d23271b545591f2b124d3192a6b281f70b
Epam <epam@Vitalis-MacBook-Pro.local>
2020-01-12 20:34:17 -0500

    in progress

---
+++ FS_Xamarin_Financial_Application/DashBoardPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace FS_Xamarin_Financial_Application
+{
+    public class DashBoardPage : ContentPage
+    {
+        public DashBoardPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
